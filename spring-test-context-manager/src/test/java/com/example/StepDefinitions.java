0edf549853dc7318c82a56b6436de3d618daa81c
M.P. Korstanje <rien.korstanje@gmail.com>
2020-02-28 17:26:09 +0100

    Test component example

--- spring-test-context-manager/src/test/java/com/example/StepDefinitions.java
+++ spring-test-context-manager/src/test/java/com/example/StepDefinitions.java
@@ -4,18 +4,20 @@ import io.cucumber.java.en.Given;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
-@DirtiesContext
 public class StepDefinitions {
     @Autowired
     private ApplicationContext applicationContext;
+    @Autowired
+    private TestComponent testComponent;
 
     @Given("an application context")
     public void anApplicationContext() {
         assertNotNull(applicationContext);
+        assertNotNull(testComponent);
+        System.out.println("Test component=" + testComponent.toString());
     }
 }
